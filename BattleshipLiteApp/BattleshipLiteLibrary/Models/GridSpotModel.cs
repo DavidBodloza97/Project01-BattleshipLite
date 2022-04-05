88e2b32772c11802face3c4968f665c105b392dd
David Bodloza <davidbodloza97@gmail.com>
2022-04-05 20:48:23 +0200

    Add class library file

---
+++ BattleshipLiteApp/BattleshipLiteLibrary/Models/GridSpotModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+    public class GridSpotModel
+    {
+        public string SpotNumber { get; set; }
+        public int SpotLetter { get; set; }
+        public GridSpotStatus Status { get; set; } = GridSpotStatus.Empty;
+    }
+}
